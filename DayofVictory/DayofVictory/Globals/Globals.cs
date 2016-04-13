7f645ea68b44603d5d7538c798c5205776bc4ebe
asr1 <alexandersrinehart@gmail.com>
2016-04-12 20:38:02 -0500

    Add screen skeleton

---
+++ DayofVictory/DayofVictory/Globals/Globals.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DayofVictory.ScreenManager;
+
+namespace DayofVictory.Globals
+{
+    public class Globals
+    {
+        //public static ScreenManager.ScreenManager screenManager = new ScreenManager.ScreenManager();
+    }
+}
+                                                   
