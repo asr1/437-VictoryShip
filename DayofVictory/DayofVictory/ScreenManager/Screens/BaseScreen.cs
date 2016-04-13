f0574665c654c95cd4879f88a17dea81a41df70b
asr1 <alexandersrinehart@gmail.com>
2016-04-12 20:08:14 -0500

    Fix Jake's break?

---
+++ DayofVictory/DayofVictory/ScreenManager/Screens/BaseScreen.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DayofVictory.ScreenManager;
+
+namespace DayofVictory.ScreenManager.Screens
+{
+    //Again with the copypasting
+    abstract class BaseScreen
+    {
+        public String name;
+        public ScreenState state;
+        public Single position;
+        public bool focused;
+        public bool grabFocus = true;
+
+        public virtual void HandleInput()
+        {
+
+        }
+
+        public virtual void Update(float delta)
+        {
+
+        }
+
+        public virtual void Draw()
+        {
+
+        }
+
+        public virtual void Unload()
+        {
+            state = ScreenState.Shutdown;
+        }
+
+    }
+}
