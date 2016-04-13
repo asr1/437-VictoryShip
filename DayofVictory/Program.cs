6535487c13df9ff5421eba57940e6e938e5f3af2
Jake Long <jakeprogrammer@gmail.com>
2016-04-12 19:49:36 -0500

    Restructure folder

--- DayofVictory/Program.cs
+++ DayofVictory/Program.cs
@@ -14,7 +14,7 @@ namespace DayofVictory
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new DayOfVictory())
                 game.Run();
         }
     }
